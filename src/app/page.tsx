41e58eb0daac9b2f4f8f5d1aefef2a5941e8e480
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-06-03 09:25:08 -0700

    added the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 "use client"
 
-import { SignInButton, UserButton, useAuth } from "@clerk/nextjs";
-import Image from "next/image";
-import { useCallback } from "react";
+import TypewriterText from "@components/TypewriterText";
+import { Button } from "@components/ui/button";
 
 export default function Home() {
 
- const {isSignedIn} = useAuth()
   return (
-    <div className="text-blue-300">Hello World
-    { isSignedIn ? <UserButton/> : <SignInButton/>}</div>
+    <div className="w-screen h-screen bg-gradient-to-r from-rose-100 grainy to-teal-100">
+      <div className="h-full flex flex-col justify-center items-center">
+        <h1 className="text-7xl font-semibold text-center">AI <span className="text-green-600 font-bold">note-taking</span> {" "}
+          assistant</h1>
+        <div className="mt-4"/>
+        <h2 className="text-4xl"><TypewriterText/></h2>
+        <div className="mt-8"/>
+        <Button className="bg-green-600">Get started</Button>
+      </div>
+    </div>
   );
 }
